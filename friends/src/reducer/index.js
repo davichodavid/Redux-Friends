f9aef25e7722800422e5d3e32bb136f56eae9aea
davichodavid <davichodavid@yahoo.com>
2019-05-16 17:00:51 -0400

    Reducers for login created, can login successfully, need to route based on success

--- friends/src/reducer/index.js
+++ friends/src/reducer/index.js
@@ -1,9 +1,31 @@
+import { SUBMITTING, SUBMITTING_SUCCESS, SUBMITTING_FAILURE } from '../actions'
+
 const initialState = {
-  friends: []
+  friends: [],
+  isLoading: false,
+  error: ''
 }
 
 export const rootReducer = (state = initialState, action) => {
   switch (action.type) {
+    case SUBMITTING:
+      return {
+        ...state,
+        isLoading: true,
+        error: ''
+      }
+    case SUBMITTING_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        error: ''
+      }
+    case SUBMITTING_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      }
     default:
       return state;
   }
